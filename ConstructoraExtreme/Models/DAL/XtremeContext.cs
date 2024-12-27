32700d95a8762d1a595cd63c1b8ba824bb57655c
yeraldycarpio <2430933@clases.edu.sv>
2024-12-26 18:32:31 -0600

    instalacion de package y clase DbContext

---
+++ ConstructoraExtreme/Models/DAL/XtremeContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace ConstructoraExtreme.Models.DAL
+{
+    public class XtremeContext : DbContext
+    {
+        public XtremeContext(DbContextOptions<XtremeContext> options) : base(options)
+        {
+        }
+    }
+
+}
