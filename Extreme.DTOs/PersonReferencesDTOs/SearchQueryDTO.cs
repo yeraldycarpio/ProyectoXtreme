efa78a6bfe385c724f34a9a13685b74cf3114b10
yeraldycarpio <2430933@clases.edu.sv>
2025-01-21 17:18:42 -0600

    DTOs PersonReferences

---
+++ Extreme.DTOs/PersonReferencesDTOs/SearchQueryDTO.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extreme.DTOs.PersonReferencesDTOs
+{
+    internal class SearchQueryDTO
+    {
+    }
+}
