3f99a84b41ff1f303ae9a91f6e52b84f66cbb9bf
mm24005 <nerydavidmendezmendez@gmail.com>
2025-01-30 20:32:39 -0600

    MaintenanceRecords DTOs

---
+++ Extreme.DTOs/MaintenanceRecordsDTOs/CreateMaintenanceRecordsDTO.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extreme.DTOs.MaintenanceRecordsDTOs
+
+{
+    public class CreateMaintenanceRecordsDTO
+    {
+
+        [Display(Name = "Maintenance_Id ")]
+        [Required(ErrorMessage = "Maintenance_Id  es requerida")]
+        public int Maintenance_Id { get; set; }
+
+        [Display(Name = "Task_Name")]
+        [Required(ErrorMessage = "The Task_Name is required.")]
+        public string Task_Name { get; set; }
+
+        [Display(Name = "Description")]
+        [Required(ErrorMessage = "The Description is required.")]
+        public string Description { get; set; }
+
+        [Display(Name = "Status")]
+        [Required(ErrorMessage = "The Status is required.")]
+        public string Status { get; set; }
+
+    }
+
+}
---
+++ Extreme.DTOs/MaintenanceRecordsDTOs/SearchResultMaintenanceRecordsDTO.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extreme.DTOs.MaintenanceRecordsDTOs
+{
+    public class SearchResultMaintenanceRecordsDTO
+    {
+        public int CountRow { get; set; }
+
+        public List<MaintenanceRecordsDTO> Data { get; set; } = new();
+
+        public class MaintenanceRecordsDTO
+        {
+
+            public int Id { get; set; }
+            public int Maintenance_Id { get; set; }
+            public string Task_Name { get; set; }
+            public string Description { get; set; }
+            public string Status { get; set; }
+        }
+    }
+
+}
